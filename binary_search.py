91facf214ceecca8f9c56891be34520e0847c5b4
Elian <elian@localhost.localdomain>
2018-04-17 11:44:41 +0100

    oop on python

--- binary_search.py
+++ binary_search.py
@@ -5,30 +5,70 @@ Created on Fri Mar 23 19:11:34 2018
 
 @author: elian
 """
-num=50
-iteractions=0
-lower_edge=1
-upper_edge=100
-mid= (upper_edge+lower_edge)//2 
-print("is your number "+str(num)+" ?")
-while True:
-    ans=input("Enter H for guess too HiGH, Enter L for guess too low and C if is correct: ").lower()
-    iteractions+=1
-   
-    if ans=="l":
-        lower_edge= mid+1
-        mid= (upper_edge+lower_edge)//2  
-        print("is your number "+str(mid)+" ? ")
-    elif ans=="h":
-        upper_edge=mid -1
-        mid = (upper_edge+ lower_edge)//2
-        print("is your number "+str(mid)+" ?")
-    elif ans== "c":
-        print("I guessed")
-        print("Numer of iteraction:"+ str(iteractions))
-        break
-      
+
+
+class QuestionAndAnswer():
+    
+    num = None
+    iteractions = 0
+    lower_edge = 1
+    upper_edge = 100
+    mid = (upper_edge+lower_edge)//2
+    
+    
+    def __init__(self,numVal = 70):
+        self.num=numVal
+
+    def handle_answer(self, inStr):
+        print ("User typed : %s" % (inStr,) )
+        print (self.mid)
+        print (self.num)
+
+    
+    def handle_Input(self):
+        ans = str(input("Enter H for guess too HiGH, Enter L for guess too low and C if is correct: ")).lower()
         
+        return ans
         
+    def handle_interactions(self):
+        self.iteractions += 1
         
+        return self.iteractions
+        
+
+    
+    def askAnswer(self):
+    
+        
+        running = True
+        while running:
+            ans = self.handle_Input()
+            
+            if ans == "l":
+                self.lower_edge = self.mid+1
+                self.mid = (self.upper_edge+self.lower_edge)//2
+                print("is your number "+str(self.mid)+" ? ")
+                self.handle_interactions()
+                
+            elif ans == "h":
+                self.upper_edge=self.mid - 1
+                self.mid = (self.upper_edge + self.lower_edge)//2
+                print("is your number "+str(self.mid)+" ?")
+                
+                self.handle_interactions()
+                
+                
+            elif ans== "c":
+                print("I guessed")
+                print("Numer of iteraction:"+ str(self.iteractions))
+                running = False
+                
+        
+        print ("Thank you, Exiting")
+        input("Good Bye")
+        #exit(0)
+            
+userCheck = QuestionAndAnswer()
+
+userCheck.askAnswer()
     
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Python projects',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='elian',
+    author_email='',
+    description=''
+)
